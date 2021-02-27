6572647d738af4faf220bdb7c431ef20f4d10318
Harsh <harsh@123.com>
2021-02-27 15:53:37 +0530

    no and math methods

---
+++ Ruby/no and math methods.rb
@@ -0,0 +1,12 @@
+num = -20
+no= 23.456
+# number methods
+puts num.abs()
+puts no.round()
+puts no.ceil()
+puts no.floor()
+puts "-------------------"
+#math methods
+A= 64
+puts Math.sqrt(A)
+puts Math.log(A)
