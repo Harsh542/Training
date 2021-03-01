36ccf754a1658d7bae447ac62dbb2f7d1847c2a9
Harsh <harsh@123.com>
2021-03-01 10:30:16 +0530

    Constants in Ruby

---
+++ Ruby/Constants.rb
@@ -0,0 +1,11 @@
+class Constant
+  #THESE(VAR1 and VAR2) ARE THE CONSTANTS------->
+  VAR1=100
+  VAR2=200
+  def show
+    puts "var1 is: #{VAR1}"
+    puts "var2 is: #{VAR2}"
+  end
+end
+c1=Constant.new
+c1.show
