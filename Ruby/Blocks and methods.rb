c1cb79dbf6279c2e4f6c9efced3504ca6e3cc42c
Harsh <harsh@123.com>
2021-03-01 13:30:20 +0530

    Blocks

---
+++ Ruby/Blocks and methods.rb
@@ -0,0 +1,9 @@
+def test
+  puts "you are in method"
+  yield 5  # This yield is used to call the block and can be used only inside the function with a same name
+  puts "you are again back in method "
+  yield 10
+end
+
+
+test { |i|  puts "you are now in block #{i}"}
