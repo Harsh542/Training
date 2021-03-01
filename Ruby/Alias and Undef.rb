26a58cb53314ffd3128bd45d9fd42186fd63036d
Harsh <harsh@123.com>
2021-03-01 13:11:55 +0530

    Alias and Undef

--- Ruby/Alias and Undef.rb
+++ Ruby/Alias and Undef.rb
@@ -1,11 +1,11 @@
 def name(nam)
   puts "hello how are you #{nam}"
 end
-alias n name  # This will create an alias for the name method
+alias n name  # This will create an alias n for the name method
 n "Harsh"
 name "Rahul"
 
 puts "----------------"
 undef name   # This will undefined the name method
 n "Karan"
-name "Rohit" # This will give error we have undefined the name method
+name "Rohit" # This will give error as we have undefined the name method
