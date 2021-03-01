7f1716bbfcd6c04194498a7c5628c43fab560706
Harsh <harsh@123.com>
2021-03-01 12:35:54 +0530

    Methods in ruby

--- Ruby/methods in ruby.rb
+++ Ruby/methods in ruby.rb
@@ -3,6 +3,8 @@ def person(name,age,country)
   puts "yous Age is :#{age}"
   puts "your country is :#{country}"
 end
+
+
 person      "karan",26,"India"
 puts "--------------------------------"
 person( "harsh", 25,"India")
