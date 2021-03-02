221d2fd865bce16966fd69bc99587eafb29e5dbb
Harsh <harsh@123.com>
2021-03-02 18:18:49 +0530

    Prime no

---
+++ Ruby/prime No.rb
@@ -0,0 +1,24 @@
+def prime(n)
+  flag=0
+  if n<0
+    puts "Please enter positive no"
+  end
+  for i in 2..n/2
+    if n%i == 0
+      puts "#{n} is not a prime no"
+      flag=1
+      break
+    end
+  end
+  if flag==0
+    puts "#{n} is a prime no"
+  end
+end
+
+ans='N'
+while ans=='N'
+print "Enter a no to if it is a prime no or not:"
+prime gets.chomp.to_i
+print "you want to exit or not (Y/N): "
+ans=gets.chomp
+end
