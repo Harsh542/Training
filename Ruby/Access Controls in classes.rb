6273ece7175d0c37994ac70874b672a4942fd975
Harsh <harsh@123.com>
2021-03-03 18:15:37 +0530

    Access Controls

---
+++ Ruby/Access Controls in classes.rb
@@ -0,0 +1,29 @@
+class Box
+  def initialize(w,h)
+    @width= w
+    @height=h
+  end
+
+  def getArea # ---> This is a public method
+    getwidth * getheight
+  end
+  def getwidth #--->it is private method
+    @width
+  end
+  def getheight #----->it is also private method
+    @height
+  end
+  private :getwidth ,:getheight  #----> making these two methods Private
+
+  def printArea
+    @area=getheight * getwidth
+    puts "the area of inside is :#@area"
+  end
+    protected :printArea  #---> making printArea method to Protected
+end
+
+box=Box.new(10,20)
+x=box.getArea  #------>This will not show any error because the getArea method is public
+puts "the area of outside is:#{x}"
+
+box.printArea  #------> This will show error because the printArea method is protected and can only be called by Box's subclass.
