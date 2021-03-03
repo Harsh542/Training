624f3cb0af532b7cc1ff04041d2c09d5d0be91b7
Harsh <harsh@123.com>
2021-03-03 11:36:34 +0530

    Read File

--- Ruby/Reading from a file.rb
+++ Ruby/Reading from a file.rb
@@ -8,3 +8,16 @@ else
   puts "unable to open the file"
 end
 aFile.close
+
+
+# <--------------File opening using File.open() Method------------->
+
+File.open("input1.txt","r") do |aFile|
+  if aFile
+    content=aFile.sysread(50)
+    puts content
+  else
+    puts "Unable to open the file"
+  end
+end
+
