7ce5ff60fb2cb71ae7904c3bebe7716686b3125c
Harsh <harsh@123.com>
2021-03-05 18:05:35 +0530

    armstrong no

---
+++ Ruby/Armstrong No.rb
@@ -0,0 +1,22 @@
+def arm(n)
+  temp=n
+  a=n.to_s
+  pow=a.length
+  sum=0
+  while n>0
+    s=n%10
+
+    sum=sum+(s**pow)
+    n=n/10
+  end
+  puts sum
+  if temp==sum
+    puts "It is armstrong no"
+  else
+    puts "Not a armstrong no"
+  end
+  end
+
+print "Enter a number to check:"
+n=gets.chomp.to_i
+arm n
